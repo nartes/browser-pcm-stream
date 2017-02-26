b8aac0959f9528b6b216b208f89ced3092f13729
Siarhei Siniak <serega.belarus@gmail.com>
2017-02-26 20:18:03 +0300

    Refactor server's logic

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
 var wav = require('wav');
+var net = require('net');
 
 const EXTERNAL_APP_HOST = process.env['EXTERNAL_APP_HOST'] || 'localhost';
 const APP_PORT = process.env['APP_PORT'] || 3700;
@@ -27,27 +28,51 @@ app.listen(APP_PORT);
 
 console.log('server open on port ' + APP_PORT);
 
-binaryServer = BinaryServer({port: WEB_SOCK_PORT});
+(() => {
+  var web_socket_client = null;
+  var web_socket_stream = null;
+  var socket_client = null;
 
-binaryServer.on('connection', function(client) {
-  console.log('new connection');
+  var web_socket_client_cb = (client) => {
+    console.log('new connection');
+    web_socket_client = client;
 
-  client.on('stream', function(stream, meta) {
-    console.log('new stream');
+    web_socket_client.on('stream', function(stream, meta) {
+      console.log('new stream');
+      web_socket_stream = stream;
 
-    server = require('net').createServer((c) => {
-      console.log('internal socket client');
+      step();
+    });
+  };
 
-      stream.pipe(c);
+  var socket_client_cb = (client) => {
+    console.log('internal socket client');
+    socket_client = client;
 
-      stream.on('end', () => {
-        c.end();
+    step();
+  };
+
+  var step = (() => {
+    return () => {
+      if (web_socket_stream == null || web_socket_client == null ||
+          socket_client == null) {
+        return;
+      }
+
+      web_socket_stream.pipe(socket_client);
+
+      web_socket_stream.on('end', () => {
+        socket_client.end();
         console.log('internal closed connection');
       });
-    });
+    }
+  })();
 
-    server.listen(INTERNAL_SOCK_PORT, () => {
-      console.log('server bound');
-    });
+  internal_server = net.createServer(socket_client_cb);
+  internal_server.listen(INTERNAL_SOCK_PORT, EXTERNAL_APP_HOST, () => {
+    console.log('internal server bound');
   });
-});
+
+  binaryServer = BinaryServer({host: EXTERNAL_APP_HOST, port: WEB_SOCK_PORT});
+  binaryServer.on('connection', web_socket_client_cb);
+})()
